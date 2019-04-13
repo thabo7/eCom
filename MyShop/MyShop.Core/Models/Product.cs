3ebb1b71d0ff4a2bcd828d2e123cc5642bcfa740
Thabo Excellent Nkosi <30670932+thabo7@users.noreply.github.com>
2019-04-13 23:14:54 +0200

    Product Model and InMemory Product Repository Built

---
+++ MyShop/MyShop.Core/Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.Core.Models
+{
+   public class Product
+    {
+        public string Id { get; set; }
+
+        [StringLength(20)]  //this adds restriction on the name 
+        [DisplayName (" Product Name")]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(0,1000)] //the range of the price 
+        public decimal price { get; set; }
+        public string Category { get; set; }
+        public string Image { get; set; }
+
+        public Product()
+        {
+            this.Id = Guid.NewGuid().ToString();
+
+        }
+
+
+    }
+}
---
+++ MyShop/MyShop.DataAccess.InMemory/ProductRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using MyShop.Core.Models;
+
+namespace MyShop.DataAccess.InMemory
+{
+  public  class ProductRepository
+    {
+
+        ObjectCache cache = MemoryCache.Default;
+        List<Product> products ;
+        public ProductRepository() //this contract will do a standard initialization 
+        {
+            products = cache["products"] as List<Product>;
+            if(products == null)
+            {
+                products = new List<Product>();
+            }
+
+
+        }
+        public void Commit() //when people adds product we don't want to add the product straight away
+        {
+            cache["Products"] = products; 
+        }
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id); // searching the product that needs to be updated
+            if(productToUpdate !=null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+
+        }
+
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == p.Id); 
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+        }
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+        public void Delete(string Id)
+        {
+            Product productToDelete = products.Find(p => p.Id == Id); // searching the product that needs to be updated
+            if (productToDelete != null)
+            {
+               products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Product not found");
+            }
+
+        }
+    }
+}
