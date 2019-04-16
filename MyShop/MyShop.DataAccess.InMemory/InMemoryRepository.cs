54b684ea9dcae9340fdd1fc0769b06f00537d825
Thabo Excellent Nkosi <30670932+thabo7@users.noreply.github.com>
2019-04-16 18:09:39 +0200

    Dependency Injection Implemented

--- MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using MyShop.Core.Models;
+﻿using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.InMemory
 {
-  public  class InMemoryRepository<T> where T : BaseEntity
+  public  class InMemoryRepository<T> : IRepository<T> where T : BaseEntity
     {
         ObjectCache cache =MemoryCache.Default;
         List<T> items;
@@ -27,6 +28,7 @@ namespace MyShop.DataAccess.InMemory
         {
             cache[ClassName] = items;
         }
+     
 
         public void Insert(T t)
         {
