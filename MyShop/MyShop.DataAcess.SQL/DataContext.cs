f239907c90db3cb638aef7c5a3cef53cb8d80c3f
Thabo Excellent Nkosi <30670932+thabo7@users.noreply.github.com>
2019-04-23 01:19:36 +0200

    Registering customers implemented

--- MyShop/MyShop.DataAcess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAcess.SQL/DataContext.cs
@@ -19,6 +19,7 @@ namespace MyShop.DataAcess.SQL
         public DbSet<ProductCategory> ProductCategories { get; set; }
         public DbSet<Basket> Baskets { get; set; }
         public DbSet<BasketItem> BasketItems { get; set; }
+        public DbSet<Customer> Customers { get; set; }
 
     }
 }
