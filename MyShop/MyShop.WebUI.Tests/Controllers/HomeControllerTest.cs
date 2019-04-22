14e1e99b85f619e91b8fd20a4ff4ddb4a09ac759
Thabo Excellent Nkosi <30670932+thabo7@users.noreply.github.com>
2019-04-22 23:59:06 +0200

    Basket Testing Implemented

--- MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
+++ MyShop/MyShop.WebUI.Tests/Controllers/HomeControllerTest.cs
@@ -4,51 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using MyShop.WebUI;
 using MyShop.WebUI.Controllers;
 
 namespace MyShop.WebUI.Tests.Controllers
 {
     [TestClass]
-    public class HomeControllerTest
+    public class UnitTest1
     {
-        //[TestMethod]
-        //public void Index()
-        //{
-        //    // Arrange
-        //    HomeController controller = new HomeController();
+        [TestMethod]
+        public void IndexPageDoesReturnProducts()
+        {
+            IRepository<Product> productContext = new Mocks.MockContext<Product>();
+            IRepository<ProductCategory> productCategoryContext = new Mocks.MockContext<ProductCategory>();
 
-        //    // Act
-        //    ViewResult result = controller.Index() as ViewResult;
+            productContext.Insert(new Product());
 
-        //    // Assert
-        //    Assert.IsNotNull(result);
-        //}
 
-        //[TestMethod]
-        //public void About()
-        //{
-        //    // Arrange
-        //    HomeController controller = new HomeController();
+            HomeController controller = new HomeController(productContext, productCategoryContext);
 
-        //    // Act
-        //    ViewResult result = controller.About() as ViewResult;
+            var results = controller.Index() as ViewResult;
+            var viewModel = (ProductListViewModel)results.ViewData.Model;
 
-        //    // Assert
-        //    Assert.AreEqual("Your application description page.", result.ViewBag.Message);
-        //}
+            Assert.AreEqual(1, viewModel.Products.Count());
 
-        //[TestMethod]
-        //public void Contact()
-        //{
-        //    // Arrange
-        //    HomeController controller = new HomeController();
-
-        //    // Act
-        //    ViewResult result = controller.Contact() as ViewResult;
-
-        //    // Assert
-        //    Assert.IsNotNull(result);
-        //}
+        }
+       
     }
 }
